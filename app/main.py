f1bcc3d2711a389cbc316442aa807b149556e0c8
Austin Kocab <austinko1128@gmail.com>
2026-02-17 20:52:42 -0500

    Add performance metrics

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, fpgrowth
@@ -16,11 +17,19 @@ df = pd.DataFrame(te_ary, columns = te.columns_)
 min_support = 500 / len(df)
 
 # Apriori
+start_apriori = time.perf_counter() 
 freq_apriori = apriori(df, min_support, use_colnames = True)
+end_apriori = time.perf_counter()
+
 print('\nFrequent itemsets (Apriori):')
 print(freq_apriori)
+print(f'\nExecution Time: {(end_apriori - start_apriori) * 1000} ms')
 
 # FP-Growth
+start_fpgrowth = time.perf_counter() 
 freq_fpgrowth = fpgrowth(df, min_support, use_colnames = True)
-print('\nFrequent itemsets (Apriori):')
+end_fpgrowth = time.perf_counter()
+
+print('\nFrequent itemsets (FP-Growth):')
 print(freq_fpgrowth)
+print(f'\nExecution Time: {(end_fpgrowth - start_fpgrowth) * 1000} ms')
