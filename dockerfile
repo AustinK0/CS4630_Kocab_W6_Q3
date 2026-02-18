b72be4b4c3b00fc945bd05afd69a89f8baea2b21
Austin Kocab <austinko1128@gmail.com>
2026-02-17 20:12:42 -0500

    readme updates

--- dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.11-slim
-
-ENV PYTHONBUFFERED=1
-
-RUN pip install pandas mlxtend
-
-COPY . .
-
-CMD ["python", "main.py"]
--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, fpgrowth
-
-print("functional")
