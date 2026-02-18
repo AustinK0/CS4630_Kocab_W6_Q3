19b8c37ca23e070008f88514b2b7ef6b48d16ae7
Austin Kocab <austinko1128@gmail.com>
2026-02-17 20:41:14 -0500

    logic complete

--- app/main.py
+++ app/main.py
@@ -2,6 +2,25 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, fpgrowth
 
+# Import initial data
 df = pd.read_csv('data.csv')
 
-print(df.head())
+# One-hot encode dataset
+transaction_groups = df.groupby('Transaction')['Item'].apply(list).reset_index()
+data = transaction_groups['Item'].tolist()
+te = TransactionEncoder()
+te_ary = te.fit(data).transform(data)
+df = pd.DataFrame(te_ary, columns = te.columns_)
+
+# rroughly 5.28%
+min_support = 500 / len(df)
+
+# Apriori
+freq_apriori = apriori(df, min_support, use_colnames = True)
+print('\nFrequent itemsets (Apriori):')
+print(freq_apriori)
+
+# FP-Growth
+freq_fpgrowth = fpgrowth(df, min_support, use_colnames = True)
+print('\nFrequent itemsets (Apriori):')
+print(freq_fpgrowth)
